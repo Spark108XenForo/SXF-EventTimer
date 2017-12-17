b30afa16407c7e6c42a0a66f3fe73c451211533f
Kirigai Asumo <spark.vat@gmail.com>
2017-12-17 20:21:12 +0300

    Add files via upload

---
+++ Setup.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SXF\ET;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+use XF\Db\Schema\Alter;
+use XF\Db\Schema\Create;
+
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+
+	public function installStep1()
+	{
+
+	}
+	
+	public function uninstallStep1()
+	{
+
+	}
+}
